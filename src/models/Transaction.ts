ffd0b186c8e7fb99e3da61b147bbc2acb0b3d8d8
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-08-24 17:53:01 -0300

    :sparkles: challenge finalized

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -26,6 +26,9 @@ class Transaction {
 
   @ManyToOne(() => Category)
   @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
   @CreateDateColumn()
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction not found');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
